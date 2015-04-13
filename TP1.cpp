68dac954b35e9ffc39c23505ccd07cb9840c337c
aguelmartin <martin.aguel@yahoo.com>
2015-04-12 22:31:07 -0300

    TP1
    AGUEL MARTIN DANIEL - K1051

---
+++ TP1.cpp
@@ -0,0 +1,33 @@
+/* TP1
+ * MARTIN DANIEL AGUEL
+ * 20150412
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+bool a;
+char b;
+int c;
+double d;
+string r0, r1 ="Sus datos", r2 = " son:";
+r0= r1+r2;
+
+cout << "Ingrese un caracter en Binario" << endl;
+cin >> a;
+cout << "Ingrese una letra" << endl;
+cin >> b;
+cout << "Ingrese un numero en Enteros (Z)" << endl;
+cin >> c;
+cout << "Ingrese un numero en reales (R)" << endl;
+cin >> d;
+
+cout << r0 << endl;
+cout << a << endl;
+cout << b << endl;
+cout << c << endl;
+cout << d << endl;
+
+}
